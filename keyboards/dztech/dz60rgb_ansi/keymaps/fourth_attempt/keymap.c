2f85bf21e45f027b2be9bffeeba9bf0c4ae6e7c6
Michael Gaddis <gaddimd@gmail.com>
2020-01-05 15:19:15 -0600

    Another round.

--- keyboards/dztech/dz60rgb_ansi/keymaps/fourth_attempt/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/fourth_attempt/keymap.c
@@ -5,13 +5,13 @@
 #define CTL_RGT RCTL_T(KC_RGHT)
 #define SFT_UP  RSFT_T(KC_UP)
 
-
 enum custom_keycodes
 {
   DSK_LFT = SAFE_RANGE,
   DSK_RGT,
   SLC_L2R,
   SLC_R2L,
+  CTALDEL,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
@@ -46,6 +46,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
         SEND_STRING(SS_DOWN(X_LSFT) SS_TAP(X_HOME) SS_UP(X_LSFT));
       } 
       break;
+      
+    case CTALDEL:
+      if (record->event.pressed)
+      {
+        SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LALT) SS_TAP(X_DEL) SS_UP(X_LCTL) SS_UP(X_LALT));
+      } 
+      break;
 
   }
   return true;
@@ -76,15 +83,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
   MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
   KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          SFT_UP,
-  KC_LCTL, KC_LALT, MO(1),                              KC_SPC,                             MO(2),   GUI_LFT, ALT_DWN, CTL_RGT),
+  KC_LCTL, MO(1),   KC_LALT,                            KC_SPC,                             MO(2),   GUI_LFT, ALT_DWN, CTL_RGT),
   
 	[1] = LAYOUT_60_ansi(
 	
 	KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
   KC_NO,   KC_HOME, KC_END,  KC_NO,   KC_NO,   KC_PSCR, KC_NO,   KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_NO,   DM_PLY1, DM_PLY2,
-  KC_TRNS, DSK_LFT, DSK_RGT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_HOME, KC_END,  SLC_R2L, SLC_L2R, KC_NO,            KC_NO,   
-  KC_LSFT,          KC_PGUP, KC_PGDN, KC_NO,   KC_NO,   KC_NO,   KC_PGUP, KC_PGDN, KC_NO,   KC_NO,   KC_NO,            KC_RSFT,   
-  KC_LCTL, KC_LALT, KC_TRNS,                            KC_MPLY,                            KC_NO,   KC_RGUI, KC_RALT, KC_RCTL),
+  KC_TRNS, DSK_LFT, DSK_RGT, KC_NO,   CTALDEL, KC_NO,   KC_NO,   KC_HOME, KC_END,  SLC_R2L, SLC_L2R, KC_NO,            KC_NO,   
+  KC_LSFT,          SLC_R2L, SLC_L2R, KC_NO,   KC_NO,   KC_NO,   KC_PGUP, KC_PGDN, KC_NO,   KC_NO,   KC_NO,            KC_RSFT,   
+  KC_LCTL, KC_TRNS, KC_LALT,                            KC_MPLY,                            KC_NO,   KC_RGUI, KC_RALT, KC_RCTL),
 	
 	[2] = LAYOUT_60_ansi(
 	
